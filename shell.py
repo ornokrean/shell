925e52184497f10d6d113b9ae9bf6e0bc4a6c852
ornokrean <or.nokrean@mail.huji.ac.il>
2019-06-10 11:12:56 +0300

    sa

--- shell.py
+++ shell.py
@@ -1,20 +1,18 @@
 from pathlib import Path
 import os
 
+
 class Shell:
     currPath = str(Path.home())
     currCommand = ""
 
-
-
-
     def pwd(self):
         print(self.currPath)
 
-#############################################################################
+    ###########################################################################
 
     def cd(self):
-        #os.path.join(
+        # os.path.join(
         pass
 
     def verifyAddress(self):
@@ -23,19 +21,19 @@ class Shell:
         #  check for abs or rel: if i have this sub path from this one ok.
         # if i dont so i check from base. otherwise error.
 
-
-    def ls(self,args):
-        allFiles = [f for f in os.listdir(self.currPath)  if not
-            f.startswith('.')]
+    def ls(self, args):
+        all_files = [f for f in os.listdir(self.currPath) if not
+        f.startswith('.')]
         if "a" in args:
             # Get hidden files:
-            allFiles = [f for f in os.listdir(self.currPath)]
+            all_files = [f for f in os.listdir(self.currPath)]
         if "t" in args:
             # Order by creation time:
-            allFiles.sort(key=os.path.getctime)
-        for file in allFiles:
+            mtime = lambda f: os.stat(os.path.join(self.currPath, f)).st_mtime
+            all_files = list(sorted(all_files, key=mtime))
+        # all_files.sort(key=os.path.getctime)
+        for file in all_files:
             print(file)
-
 
     def cp(self, args, source, dest):
         # check source and dest
@@ -43,10 +41,7 @@ class Shell:
         pass
 
 
-
 s = Shell()
 
 s.pwd()
-s.ls(["a","t"])
-
-
+s.ls(["a", "t"])
